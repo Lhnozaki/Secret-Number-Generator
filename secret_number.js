70e33829fdb01bfd54edae87c0c4fb1a174be9f2
Lewis Nozaki <lhnozaki@gmail.com>
2019-07-02 21:53:46 -1000

    get random num, store num, call stored num

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,25 @@
 'use strict';
 module.exports = function() {
-  
+
+  // Create Random Number
+  let secretNumber = function() {
+    return Math.floor(Math.random() * 20)
+  };
+
+  // Store Random number in variable
+  let generatedNumber = secretNumber();
+
+  // Create function to call stored number
+  let storedNumber = function() {
+    return generatedNumber;
+  }
+
+  return storedNumber;
+
 };
+
+
+
+
+
+
